2d9f76c32756d1dee7aaae1777781619731ab1b1
Federico Bottoni <feder.bottoni@gmail.com>
2019-06-16 17:41:33 +0200

    fix dataset

--- src/dsShuffle.py
+++ src/dsShuffle.py
@@ -2,34 +2,41 @@ import pandas as pd
 import csv
 from random import shuffle
 from pgmNetwork import pgmNetwork
+from Snapshot import Snapshot
 from Config import Config
 
 def dsShuffle():
     print('LOG: Shuffling the dataset')
     data=list()
 
-    with open('data/Dataset.csv', mode='r', newline="\r\n", encoding="utf-8") as csv_file:
+    with open('data/FixedDataset.csv', mode='r', newline="\r\n", encoding="utf-8") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=';', quotechar='"', lineterminator='\r\n')
+        firstLine = True
         for row in csv_reader:
+            if not firstLine:
                 data.append(row)
+            else:
+                head = row
+                firstLine = False
 
-    header=data[:2]
-    data=data[2:]
     shuffle(data)
-    data=header+data
 
     with open('data/Shuffled.csv', mode='w+', newline="\n", encoding="utf-8") as csv_file:
         csv_writer = csv.writer(csv_file, delimiter=';', quotechar='"', lineterminator='\r\n', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(head) 
         for row in data:
             csv_writer.writerow(row) 
 
     print('LOG: Parsing data')
-    # parse data according to the library
-    parsedData = []
+    snapshots = list()
+    for row in data:
+        snapshots.append(Snapshot(row))
 
+    parsedData = list()
     print('LOG: Discretizing the dataset')
     config = Config()
-    # discretize data using range
+    for row in snapshots:
+        parsedData.append(row.getDiscretizedInstance(config.getRangeSize()))
 
     pgmNetwork(parsedData)
     
