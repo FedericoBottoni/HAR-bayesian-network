e138081008d793f11db843a1584d6216cd3b9b88
m-artifoni <m.artifoni@campus.unimib.it>
2019-06-28 15:50:37 +0200

    order of  variables fix

--- configs/Config.py
+++ configs/Config.py
@@ -8,7 +8,7 @@ class Config():
         return 5
 
     def evidences(self):
-        return ['x1', 'x2', 'x3', 'x4', 'y1', 'y2', 'y3', 'y4', 'z1', 'z2', 'z3', 'z4']
+        return ['x1', 'y1', 'z1', 'x2', 'y2', 'z2', 'x3', 'y3', 'z3', 'x4', 'y4', 'z4']
 
     def variables(self):
         vrbls = self.evidences()
